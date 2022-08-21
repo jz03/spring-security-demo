62e2ca75ec0efd9e9ef92417fea88e743eee945a
jizhou <jizhou03@gmail.com>
2022-08-21 14:56:58 +0800

    add 基于方法级别的权限控制
    @PreAuthorize注解

--- authorization/src/main/java/org/example/authorization/controller/MyController.java
+++ authorization/src/main/java/org/example/authorization/controller/MyController.java
@@ -1,13 +1,15 @@
 package org.example.authorization.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
 
-    @Secured("ROLE_abc")
+//    @Secured("ROLE_abc")
+    @PreAuthorize("hasRole('abc')")
     @RequestMapping("toMain")
     public String welcomeHome(){
         System.out.println("执行welcomeHome方法");
